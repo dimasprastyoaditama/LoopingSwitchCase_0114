594fe822cd6f3bddf3b40490f6c19a107fc3e7dd
dimasprastyoaditama <prastyod647@gmail.com>
2025-07-09 18:21:52 +0700

    menambahkan menu pilihan untuk menghitung luas persegi panjang dan lingkaran dalam fungsi main

--- file2.cpp
+++ file2.cpp
@@ -14,5 +14,14 @@ int main(){
     int panjang, lebar;
     float jejari;
 
-    
+    do{
+        cout << "Menu" << endl;
+        cout << "1. Luas Persegi Panjang" << endl;
+        cout << "2. Luas Lingkaran" << endl;
+        cout << "3. Luas Keluar" << endl;
+
+        cout << "Pilihan (1/2/3) = ";
+        cin >> pilihan;
+
+        
 }
